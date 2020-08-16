42c31883c515e9b6944675198bd57f8cfe02a890
Anna1970 <anna-1804@mail.ru>
2020-08-16 15:06:45 +0300

    hometask_4 with **

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -6,7 +6,6 @@ import androidx.core.view.marginBottom
 import androidx.core.view.marginLeft
 import androidx.core.view.marginRight
 import androidx.core.view.marginTop
-import kotlinx.android.synthetic.main.layout_bottombar.view.*
 
 fun View.setMarginOptionally(left:Int = marginLeft, top : Int = marginTop, right : Int = marginRight, bottom : Int = marginBottom) {
     val param = this.layoutParams as ViewGroup.MarginLayoutParams
