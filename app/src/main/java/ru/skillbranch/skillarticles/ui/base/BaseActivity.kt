3dcf833b05e3c5091bd9cc41afe41842b76e263b
Anna1970 <anna-1804@mail.ru>
2020-08-19 19:46:15 +0300

    hometask_4 with all **

--- app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
@@ -40,5 +40,5 @@ abstract class BaseActivity<T : BaseViewModel<out IViewModelState>> : AppCompatA
         binding.restoreUi(savedInstanceState)
     }
 
-    internal inline fun provideViewModel(arg : Any?) = ViewModelDelegate(ArticleViewModel::class.java, arg)
+    internal inline fun <reified T : ViewModel> provideViewModel(arg : Any?) = ViewModelDelegate(T::class.java, arg)
 }
