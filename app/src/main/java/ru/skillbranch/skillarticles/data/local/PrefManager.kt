0594b430f303e6f1bc0340949d9c9814732d49c4
Anna1970 <anna-1804@mail.ru>
2021-02-25 20:54:10 +0300

    hometask_9 without * 9

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -16,12 +16,6 @@ object PrefManager {
         PreferenceManager.getDefaultSharedPreferences(App.applicationContext())
     }
 
-    /*var storedBoolean by PrefDelegate(false)
-    var storedString by PrefDelegate("test")
-    var storedInt by PrefDelegate(Int.MAX_VALUE)
-    var storedLong by PrefDelegate(Long.MAX_VALUE)
-    var storedFloat by PrefDelegate(100f)*/
-
     private val isauth = MutableLiveData(false)
     private val appsettings = MutableLiveData(
         AppSettings(isDarkMode = false, isBigText = false)
